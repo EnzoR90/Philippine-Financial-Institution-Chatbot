2ee7e9fee7bda92b23186716af7f3c17488e1197
Enzo Rodriguez <kentlaur@andrew.cmu.edu>
2024-09-26 16:10:04 -0400

    Pin Werkzeug version and update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -42,4 +42,6 @@ def get_response():
 # Functions for fuzzy matching and getting financial institution data remain the same as in your current code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
